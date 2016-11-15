32e3679d7e7d7c319808903fe7d701eab2b7f56e
MaLiN2223 <szyszka45@gmail.com>
2016-11-15 21:18:58 +0100

    add cross road 'api'

---
+++ Structures/CrossRoad.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class CrossRoad(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    def register(self, movable):
+        """ Registers movable object """
+        pass
