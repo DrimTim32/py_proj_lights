199890b15aa8326befd5a3617b5e3e6809948613
Marcin Malinowski <MaLiN2223@users.noreply.github.com>
2016-11-15 21:02:40 +0100

    Revert "Malin"

--- Structures/Road.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from Structures.Elbow import Elbow
-
-
-class Road(Elbow):
-    def __init__(self, size):
-        pygame.sprite.Sprite.__init__(self)
-        self.buffers = [[] * size, [] * size]
