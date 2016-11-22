459e190519ca5f9749f7518a183d1ab0d7f8e2bc
Malin <szyszka45@gmail.com>
2016-11-22 18:30:35 +0100

    second side + car prototype

--- Structures/Road.py
+++ Structures/Road.py
@@ -4,7 +4,7 @@ import pygame
 
 class Road:
     def __init__(self, ):
-        self.startNode = RoadNode([0, 0], [0, 0], lambda x: [x[0] + 10, x[1]], lambda: [0, 10])
+        self.startNode = RoadNode([2, 0], [0, 0], lambda x: [x[0] + 10, x[1]], lambda: [0, 30], True)
         current = self.startNode
         for i in range(30):
             n = current.CreateNext([current.data[0] + 1, current.data[1] + 1])
@@ -18,7 +18,7 @@ class Road:
 
 
 class RoadNode(SingleNode):
-    def __init__(self, microNodes: list, pointA, calculateB, calculateVector):
+    def __init__(self, microNodes: list, pointA, calculateB, calculateVector, strip=False):
         super().__init__(microNodes)
         self.microNodes = microNodes
         self.pointA = pointA
@@ -26,14 +26,30 @@ class RoadNode(SingleNode):
         self.calculateVector = calculateVector
         self.pointB = calculateB(pointA)
         self.vector = calculateVector()
+        self.strip = strip
 
     def CreateNext(self, micronodes):
-        node = RoadNode(micronodes, self.pointB, self.calculateB, self.calculateVector)
+        node = RoadNode(micronodes, self.pointB, self.calculateB, self.calculateVector, not self.strip)
         self.connect(node)
         return node
 
     def render(self, surface: pygame.Surface):
-        pygame.draw.line(surface, (0, 0, 0), self.pointA, self.getSecondPoint(self.pointA), 2)
+        pygame.draw.line(surface, (0, 0, 0), self.pointA, self.pointB, 2)
+        pygame.draw.line(surface, (0, 0, 0), RoadNode.movePoint(self.pointA, self.vector),
+                         RoadNode.movePoint(self.pointB, self.vector), 2)
+        if self.strip:
+            vect = [self.vector[0] / 2, self.vector[1] / 2]
+            pygame.draw.line(surface, (0, 0, 0), RoadNode.movePoint(self.pointA, vect),
+                             RoadNode.movePoint(self.pointB, vect), 2)
+        self.drawCar(surface)
 
-    def getSecondPoint(self, pointA):
-        return [pointA[0] + 10, pointA[1]]
+    @staticmethod
+    def movePoint(point, vector):
+        return [point[0] + vector[0], point[1] + vector[1]]
+
+    def drawCar(self, surface):
+        if self.microNodes[0] == 2:
+            vect = [(3 * self.vector[0]) / 4, (3 * self.vector[1]) / 4]
+            carPos = [int((self.pointB[0] - self.pointA[0]) / 2 + vect[0]),
+                      int((self.pointB[1] - self.pointA[1]) / 2 + vect[1])]
+            pygame.draw.circle(surface, (255, 0, 0), carPos, 5)
