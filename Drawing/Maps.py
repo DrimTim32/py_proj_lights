3ce498422c13833db793c72f9070d2263dd6d70c
MaLiN2223 <szyszka45@gmail.com>
2016-12-04 01:28:16 +0100

    added - sealing roads

--- Drawing/Maps.py
+++ Drawing/Maps.py
@@ -31,14 +31,14 @@ def draw_line(screen, point1: Position, point2: Position, color=BLACK):
 
 class Map:
     blockSize = 20
-    minimumOffset = 200
+    minimumOffset = 100
 
     def __init__(self):
         self.topleft = [50, 50]
-        self.top = [[[0] * 10], [[0] * 10]]
-        self.left = [[[0] * 10], [[0] * 10]]
-        self.down = [[[0] * 10], [[0] * 10]]
-        self.right = [[[0] * 10], [[0] * 10]]
+        self.top = [[[0] * 0], [[0] * 0]]
+        self.left = [[[0] * 0], [[0] * 0]]
+        self.down = [[[0] * 0], [[0] * 0]]
+        self.right = [[[0] * 0], [[0] * 0]]
         self.offsetLeft = max(len(self.left[0]) * Map.blockSize, Map.minimumOffset)
         self.offsetTop = max(len(self.top[0]) * Map.blockSize, Map.minimumOffset)
         self.offsetMiddle = max(len(self.left[0]) * Map.blockSize, len(self.right[0]) * Map.blockSize)
@@ -77,7 +77,7 @@ class Map:
         return (len(self.down[0]) + len(self.down[1])) * Map.blockSize
 
     def calculatePoints(self):
-        top_start_left = Position(self.offsetLeft + self.topleft[0], self.topleft[1])
+        top_start_left = Position(self.offsetLeft + self.topleft[0], self.offsetTop + self.topleft[1])
         top_end_left = top_start_left + Position(0, self.top_road_length)
         top_start_right = top_start_left + Position(self.top_road_width, 0)
         top_end_right = top_start_right + Position(0, self.top_road_length)
@@ -101,14 +101,25 @@ class Map:
         down_end_right = down_start_right + Position(0, self.down_road_length)
         self.downPoints = ((down_start_left, down_end_left), (down_start_right, down_end_right))
 
+    def seal(self, screen):
+        if self.left_road_length <= 0:
+            draw_line(screen, self.leftPoints[0][0], self.leftPoints[1][0])
+        if self.right_road_length <= 0:
+            draw_line(screen, self.rightPoints[0][0], self.rightPoints[1][0])
+        if self.top_road_length <= 0:
+            draw_line(screen, self.topPoints[0][0], self.topPoints[1][0])
+        if self.down_road_length <= 0:
+            draw_line(screen, self.downPoints[0][0], self.downPoints[1][0])
+
     def prepare(self, screen):
         pygame.display.set_caption("Game")
         screen.fill(WHITE)
 
     def draw(self, screen):
-        self.drawDirections(screen,[self.topPoints,self.rightPoints,self.leftPoints,self.downPoints])
+        self.drawDirections(screen, [self.topPoints, self.rightPoints, self.leftPoints, self.downPoints])
+        self.seal(screen)
 
-    def drawDirections(self,screen,directions):
+    def drawDirections(self, screen, directions):
         for dir in directions:
             draw_line(screen, dir[0][0], dir[0][1])
             draw_line(screen, dir[1][0], dir[1][1])
