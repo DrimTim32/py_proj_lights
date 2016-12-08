bc89d910b340d20155fae1fd14b642f8719abf56
Malin <szyszka45@gmail.com>
2016-12-09 00:50:35 +0100

    testing continious integration p5

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="py_proj_lights",
+    version="0.0.1",
+    description="",
+    py_modules=["Drawing","Simulation"],
+    install_requires=["pygame"]
+)
