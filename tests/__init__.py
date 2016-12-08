cc6122e7f98465fcc8ce12250b1238442e9ea9b7
Malin <szyszka45@gmail.com>
2016-12-09 00:03:21 +0100

    peparation for testing

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_Position import *
---
+++ tests/test_Position.py
@@ -0,0 +1,11 @@
+# content of ./test_smtpsimple.py
+import pytest
+
+@pytest.fixture
+def smtp():
+    import smtplib
+    return smtplib.SMTP("smtp.gmail.com")
+
+def test_ehlo(smtp):
+    response, msg = smtp.ehlo()
+    assert response == 250
