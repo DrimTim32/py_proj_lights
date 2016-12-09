827dc37ac1944f4a3ec6f0f6f05c08ef36a9a8c6
Malin <szyszka45@gmail.com>
2016-12-09 01:06:00 +0100

    testing continious integration p7 (sudo needed)

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="py_proj_lights",
-    version="0.0.1",
-    description="",
-    py_modules=["Drawing","Simulation"],
-    install_requires=["pygame"]
-)
