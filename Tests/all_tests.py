593c6e06e3ca7e2e3a381da38b0319bad4edd73a
Malin <szyszka45@gmail.com>
2016-12-09 01:27:17 +0100

    travis test

--- Tests/all_tests.py
+++ Tests/all_tests.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 from Drawing.tests import *
+import pytest
+def f():
+    raise SystemExit(1)
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+with open("requirements.txt", 'r') as f:
+    requirements = f.read().split('\n')
+
+setup(
+    name='py_proj_lights',
+    version='1.0.0',
+    packages=['Drawing', 'Simulation'],
+    url='https://github.com/DrimTim32/py_proj_lights',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=requirements
+)
