d8220d479d97baab6be0bca2252db9a5f6c694a5
Malin <szyszka45@gmail.com>
2016-12-09 03:18:26 +0100

    first working tests

--- tests/test_Position.py
+++ tests/test_Position.py
@@ -1,11 +1,15 @@
-# content of ./test_smtpsimple.py
+from Drawing.DataStructures.Position import Position
 import pytest
 
-@pytest.fixture
-def smtp():
-    import smtplib
-    return smtplib.SMTP("smtp.gmail.com")
+data = [
+    (Position(1, 1), Position(1, 1), Position(2,2)),
+    (Position(2, 2), Position(2, 2), Position(4,4)),
+    (Position(3, 7), Position(3, 7), Position(6,14)),
+    (Position(7, 3), Position(7, 3), Position(14,6)),
 
-def test_ehlo(smtp):
-    response, msg = smtp.ehlo()
-    assert response == 250
+]
+
+
+@pytest.mark.parametrize("a,b,expected", data)
+def test_sum(a, b, expected):
+    assert (a + b) == expected
