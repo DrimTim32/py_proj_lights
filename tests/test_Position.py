9ac5cb2186660bcdc11ee9401e7521fda629da26
Malin <szyszka45@gmail.com>
2016-12-09 23:58:54 +0100

    updated setup file, added new branch for testing new tests for Position class

--- tests/test_Position.py
+++ tests/test_Position.py
@@ -1,15 +1,40 @@
 from Drawing.DataStructures.Position import Position
 import pytest
 
-data = [
-    (Position(1, 1), Position(1, 1), Position(2,2)),
-    (Position(2, 2), Position(2, 2), Position(4,4)),
-    (Position(3, 7), Position(3, 7), Position(6,14)),
-    (Position(7, 3), Position(7, 3), Position(14,6)),
 
-]
+@pytest.mark.parametrize("a", (0, 10, 15))
+@pytest.mark.parametrize("b", (0, 3, 5))
+@pytest.mark.parametrize("c", (0, 7, 12))
+@pytest.mark.parametrize("d", (0, 3, 9))
+def test_sum(a: int, b: int, c: int, d: int):
+    assert Position(a, b) + Position(c, d) == Position(a + c, b + d)
 
 
-@pytest.mark.parametrize("a,b,expected", data)
-def test_sum(a, b, expected):
-    assert (a + b) == expected
+@pytest.mark.parametrize("a", (0, 1, 3))
+@pytest.mark.parametrize("b", (0, 1, 5))
+@pytest.mark.parametrize("c", (0, 1, 7))
+@pytest.mark.parametrize("d", (0, 1, 8))
+def test_substraction(a: int, b: int, c: int, d: int):
+    assert Position(a, b) - Position(c, d) == Position(a - c, b - d)
+
+
+@pytest.mark.parametrize("a", (0, 1, 2, 3, 4, 5))
+@pytest.mark.parametrize("b", (0, 6, 7, 8, 11, 8))
+def test_negation(a: int, b: int):
+    assert -Position(a, b) == Position(-a, -b)
+
+
+@pytest.mark.parametrize("a", (0, 1, 2, 3, 4, 5))
+@pytest.mark.parametrize("b", (0, 6, 7, 8, 11, 8))
+def test_get_item(a: int, b: int):
+    assert Position(a, b)[0] == a
+    assert Position(a, b)[1] == b
+
+
+@pytest.mark.parametrize("a", (0, 1, 2, 3, 4, 5))
+@pytest.mark.parametrize("b", (0, 6, 7, 8, 11, 8))
+def test_copy(a: int, b: int):
+    p1 = Position(a, b)
+    p2 = p1.copy()
+    assert not (p1 is p2)
+    assert p1 == p2
