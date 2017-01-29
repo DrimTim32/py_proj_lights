d21ff5262629dd52f0d149b1a4ea052858c81640
MaLiN2223 <szyszka45@gmail.com>
2017-01-29 22:03:00 +0100

    catalogs disambiguation; added scripts and configurations files

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from core.data_structures import Position
-
-
-def pytest_assertrepr_compare(op, left, right):
-    if isinstance(left, Position) and isinstance(right, Position) and op == "==":
-        return ['Comparing Position instances:',
-                '   vals: %s != %s' % (left, right)]
