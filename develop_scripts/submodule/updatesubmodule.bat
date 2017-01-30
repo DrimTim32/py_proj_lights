7a2ad41b291a655c1d74235918133bf0e3a85212
MaLiN2223 <szyszka45@gmail.com>
2017-01-30 19:05:42 +0100

    update git module

--- develop_scripts/submodule/updatesubmodule.bat
+++ develop_scripts/submodule/updatesubmodule.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 cd ../..
+echo "Updating git submodule..."
 git submodule update
 cd develop_scripts/submodule/
+echo "Executing copying script.."
 PowerShell.exe -file UpdateSubMOdule.ps1
