0b1d78f8116afd61fa111120eba21d741898356b
Anna Gese <anna.gese@gmail.com>
2017-02-04 22:07:06 +0100

    reading config.json

--- core/configuration/config.py
+++ core/configuration/config.py
@@ -2,12 +2,40 @@ import json
 
 
 class Config:
-    def __init__(self):
-        pass
+    """
+    Configuration class
+    """
+    def __init__(self, numberOfDirections, directions, norm, variance, importance):
+        self.numberOfDirections = numberOfDirections
+        self.directions = directions
+        self.norm = norm
+        self.variance = variance
+        self.importance = importance
 
     @staticmethod
-    def from_config_file(file):
-        f = open(file)
-        data = json.load(f)
-        #TODO
-        return Config()
+    def from_config_file(file_name):
+        """
+        :param file_name: name of file with configuration
+        :type file_name: str
+        :return: Config object constructed from given file
+        :rtype: Config
+        """
+        file = open(file_name)
+        data = json.load(file)
+        return Config(data['numberOfDirections'], data['directions'], data['norm'], data['variance'], data['importance'])
+
+    @property
+    def directions_lanes(self):
+        """
+        :return: returns crossroad topology
+        :rtype: dict[int,tuple[int,int]]
+        """
+        return {direction.get('Id'): [direction.get('InLanes'), direction.get('OutLanes')] for direction in self.directions}
+
+    @property
+    def directions_turns(self):
+        """
+        :return: returns crossroad turns topology
+        :rtype: dict[int,dict[int:dict[int:[float, bool]]]]
+        """
+        return {direction.get('Id'): {lane.get("LaneId"): {turn.get('Direction'): [turn.get('Probability'), turn.get('Safe')]  for turn in lane.get('TurnDirections')} for lane in direction.get('Lanes')} for direction in self.directions}
