86059821971ec23fecaf78e5fcdd95d6de582916
MaLiN2223 <szyszka45@gmail.com>
2017-02-07 14:22:37 +0100

    more tests!

--- tests/test_drawing_helpers.py
+++ tests/test_drawing_helpers.py
@@ -17,16 +17,17 @@ from drawing.drawing_helpers import draw_car_by_value, draw_car
 class MockingTestTestCase(unittest.TestCase):
     @patch('drawing.drawing_helpers.draw_car')
     def test_mock_stubs(self, draw_car):
-        draw_car_by_value(None, 30, -1)
-        draw_car_by_value(None, 30, 0)
-        draw_car_by_value(None, 30, 1)
-        draw_car_by_value(None, 30, 2)
-        draw_car_by_value(None, 30, 3)
+        draw_car_by_value(None, 1, -1)
+        draw_car_by_value(None, 2, 0)
+        draw_car_by_value(None, 3, 1)
+        draw_car_by_value(None, 4, 2)
+        draw_car_by_value(None, 5, 3)
+        draw_car_by_value(None, 6, 4)
         calls = draw_car.mock_calls
-        assert len(calls) == 5
-        print(calls)
-        draw_car.assert_any_call(None, 30, (255, 255, 255))
-        draw_car.assert_any_call(None, 30, (200, 0, 0))
-        draw_car.assert_any_call(None, 30, (0, 200, 0))
-        draw_car.assert_any_call(None, 30, (0, 0, 200))
-        draw_car.assert_any_call(None, 30, (255, 0, 114))
+        assert len(calls) == 6
+        draw_car.assert_any_call(None, 1, (255, 255, 255))
+        draw_car.assert_any_call(None, 2, (200, 0, 0))
+        draw_car.assert_any_call(None, 3, (0, 200, 0))
+        draw_car.assert_any_call(None, 4, (0, 0, 200))
+        draw_car.assert_any_call(None, 5, (255, 0, 114))
+        draw_car.assert_any_call(None, 6, (0, 0, 0))
