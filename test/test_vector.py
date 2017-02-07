a65a948adfa6e2f5321518f99cc7ca504e7a8946
MaLiN2223 <szyszka45@gmail.com>
2017-02-07 01:18:28 +0100

    tests paths fix

--- test/test_vector.py
+++ test/test_vector.py
@@ -1,13 +1,8 @@
 import pytest
-import sys, os
-
-myPath = os.path.dirname(os.path.abspath(__file__))
+import sys
 
 if "core" not in sys.path[0]:
-    if "\\" in sys.path[0]:
-        sys.path.insert(0, 'core')
-    else:
-        sys.path.insert(0, 'core')
+    sys.path.insert(0, 'core')
 
 from data_structures import Vector
 
